298f4f3c9f57370bdc1d06e5c3baa3305bc60ab7
shital <pshital2197@gmail.com>
2022-10-21 17:45:35 +0530

    First commit

---
+++ src/test/java/com/tcs/springbootproject/Springbootproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootproject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
